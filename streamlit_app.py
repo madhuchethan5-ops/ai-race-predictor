834c04eec925f9d6dcca07c0243633e3d5fcadc0
madhuchethan5-ops <madhu.chethan5@gmail.com>
2025-12-30 16:42:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2630,7 +2630,6 @@ with Q4:
 
         st.subheader("Regret Tracker")
         st.write("Regret tracker:", st.session_state.regret_tracker)
-        st.write("Regret count:", regret_count)
         st.write("Blend weight after soft doubt:", blend_weight)
 
 # ---------------------------------------------------------
